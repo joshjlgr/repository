446a6cae2d8ec6357d8af4e3f7458b679113099d
joshjlgr <69741487+joshjlgr@users.noreply.github.com>
2020-08-16 15:15:30 +0800

    Add files via upload

---
+++ phpfiles/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body bgcolor="#f3f3f3">
+<br/>
+
+<center>
+    <h1>
+<?php
+    echo "Hello World!";
+?>
+    </h1>
+</body>
+</center>
+
+</body>
+</html>
+
+<!-- file is kept at B:\WampServer64\www -->
